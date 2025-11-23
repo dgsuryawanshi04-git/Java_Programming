d18302fa7e32ebe5d6c9e2eff5bbc24d16d22d58
dgsuryawanshi04-git <dgsuryawanshi04@gmail.com>
2025-11-23 23:32:18 +0530

    Add files via upload
    Solutions of Assignment30 (Q1 - Q5)

---
+++ Assignments/Assignment_30/program30_3.java
@@ -0,0 +1,57 @@
+
+/*
+           
+iRow: 5
+iCol: 5
+
+$ * * * *
+* $ * * *
+* * $ * *
+* * * $ * 
+* * * * $
+
+*/
+
+import java.util.*;
+class Pattern
+{
+    public void Display(int iRow, int iCol)
+    {
+        int i = 0, j = 0;
+
+        for(i = 1; i <= iRow; i++)
+        {
+            for(j = 1; j <= iCol; j++)
+            {    
+                if(i == j)
+                {
+                    System.out.print("$\t");     
+                }
+                else
+                {
+                    System.out.print("*\t");
+                }    
+            }    
+            System.out.println();    
+        }                 
+    }
+}
+
+class program30_3
+{
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+
+        int iValue1 = 0, iValue2 = 0;
+        System.out.println("Enter number of rows:");
+        iValue1 = sc.nextInt();
+
+        System.out.println("Enter number of cols:");
+        iValue2 = sc.nextInt();
+
+        Pattern pobj = new Pattern();
+        pobj.Display(iValue1, iValue2);
+        
+    }
+}
